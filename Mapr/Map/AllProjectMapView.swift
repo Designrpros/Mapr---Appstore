9c1ec5f355da369c2cdcc3e22b7361b0718fd716
Designrpros <130706554+Designrpros@users.noreply.github.com>
2023-07-04 16:11:46 +0200

    allprojectmapview, works for both macOS and ios

--- Mapr/Map/AllProjectMapView.swift
+++ Mapr/Map/AllProjectMapView.swift
@@ -1,6 +1,76 @@
 import SwiftUI
 import MapKit
 
+func isValid(region: MKCoordinateRegion) -> Bool {
+    let latitude = region.center.latitude
+    let longitude = region.center.longitude
+    let latitudeDelta = region.span.latitudeDelta
+    let longitudeDelta = region.span.longitudeDelta
+
+    if latitude >= -90 && latitude <= 90 &&
+       longitude >= -180 && longitude <= 180 &&
+       latitudeDelta > 0 && longitudeDelta > 0 {
+        return true
+    }
+    return false
+}
+
+#if os(iOS)
+struct AllProjectsMapView: UIViewRepresentable {
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Location.name, ascending: true)],
+        animation: .default)
+    private var locations: FetchedResults<Location>
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.removeAnnotations(uiView.annotations)
+        
+        print("Updating map view with locations: \(locations)")
+        
+        var minLatitude = 90.0
+        var maxLatitude = -90.0
+        var minLongitude = 180.0
+        var maxLongitude = -180.0
+
+        for location in locations {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            annotation.title = location.name
+            uiView.addAnnotation(annotation)
+
+            let latitude = location.latitude
+            let longitude = location.longitude
+            
+            minLatitude = min(minLatitude, latitude)
+            maxLatitude = max(maxLatitude, latitude)
+            minLongitude = min(minLongitude, longitude)
+            maxLongitude = max(maxLongitude, longitude)
+        }
+        
+        let centerLatitude = (minLatitude + maxLatitude) / 2.0
+        let centerLongitude = (minLongitude + maxLongitude) / 2.0
+        let centerCoordinate = CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude)
+        
+        let latitudeDelta = maxLatitude - minLatitude + 0.05
+        let longitudeDelta = maxLongitude - minLongitude + 0.05
+
+        let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: span)
+
+        if isValid(region: region) {
+            uiView.setRegion(region, animated: true)
+        } else {
+            print("Invalid region")
+        }
+    }
+}
+
+#elseif os(macOS)
 struct AllProjectsMapView: NSViewRepresentable {
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Location.name, ascending: true)],
@@ -53,18 +123,5 @@ struct AllProjectsMapView: NSViewRepresentable {
             print("Invalid region")
         }
     }
-    
-    func isValid(region: MKCoordinateRegion) -> Bool {
-        let latitude = region.center.latitude
-        let longitude = region.center.longitude
-        let latitudeDelta = region.span.latitudeDelta
-        let longitudeDelta = region.span.longitudeDelta
-
-        if latitude >= -90 && latitude <= 90 &&
-           longitude >= -180 && longitude <= 180 &&
-           latitudeDelta > 0 && longitudeDelta > 0 {
-            return true
-        }
-        return false
-    }
 }
+#endif
