e90d41c03fc68368c3a59a51fb8432a5a3d04201
Designrpros <130706554+Designrpros@users.noreply.github.com>
2023-07-04 16:14:25 +0200

    same for mapview and streetview

--- Mapr/Map/MapView.swift
+++ Mapr/Map/MapView.swift
@@ -1,6 +1,27 @@
 import SwiftUI
 import MapKit
 
+#if os(iOS)
+struct MapView: UIViewRepresentable {
+    var mapItem: MKMapItem
+    var addressTitle: String
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let region = MKCoordinateRegion(center: mapItem.placemark.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        uiView.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = mapItem.placemark.coordinate
+        annotation.title = addressTitle
+        uiView.addAnnotation(annotation)
+    }
+}
+#elseif os(macOS)
 struct MapView: NSViewRepresentable {
     var mapItem: MKMapItem
     var addressTitle: String
@@ -20,3 +41,4 @@ struct MapView: NSViewRepresentable {
         nsView.addAnnotation(annotation)
     }
 }
+#endif
--- Mapr/Map/StreetView.swift
+++ Mapr/Map/StreetView.swift
@@ -2,6 +2,24 @@ import SwiftUI
 import WebKit
 import CoreLocation
 
+#if os(iOS)
+struct StreetView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let urlString = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=\(coordinate.latitude),\(coordinate.longitude)&fov=90&heading=235&pitch=10&key=\(Constants.googleMapsAPIKey)"
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
+#elseif os(macOS)
 struct StreetView: NSViewRepresentable {
     var coordinate: CLLocationCoordinate2D
 
@@ -18,3 +36,4 @@ struct StreetView: NSViewRepresentable {
         }
     }
 }
+#endif
