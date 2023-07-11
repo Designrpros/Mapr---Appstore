deee1a77a9f2cecaeb6959d5250fa5218646432f
Designrpros <130706554+Designrpros@users.noreply.github.com>
2023-07-11 12:50:09 +0200

    Settingsview Nice

--- Mapr/Settingsview.swift
+++ Mapr/Settingsview.swift
@@ -13,55 +13,38 @@ struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
     @FetchRequest(entity: Project.entity(), sortDescriptors: []) var projects: FetchedResults<Project>
     
-    // Fetch the current user
-    @State private var currentUser: User = UserManager.shared.fetchCurrentUser(in: PersistenceController.shared.container.viewContext)
-    
-    // Fetch the user's name and email from Apple Sign In
-    @ObservedObject private var signInWithAppleManager = SignInWithAppleManager.shared
-    
     var body: some View {
         VStack {
             Form {
-                Section(header: Text("PROFILE")) {
-                    HStack {
-                        Text("Name")
-                        Spacer()
-                        Text(signInWithAppleManager.userName)
-                            .foregroundColor(.gray)
+                Section {
+                    Button(action: {
+                        deleteAll()
+                        showRestartAlert = true
+                    }) {
+                        Text("Delete All Projects & Contacts")
+                            .foregroundColor(.red)
                     }
-                    
-                    HStack {
-                        Text("Email")
-                        Spacer()
-                        Text(signInWithAppleManager.userEmail)
-                            .foregroundColor(.gray)
-                    }
-                    
-                    HStack {
-                        Text("Role")
-                        Spacer()
-                        Text(currentUser.role)
-                            .foregroundColor(.gray)
-                    }
-                    
-                    Section {
-                        Button(action: {
-                            deleteAll()
-                            showRestartAlert = true
-                        }) {
-                            Text("Delete All Projects & Contacts")
-                                .foregroundColor(.red)
-                        }
-                    }
-                }.padding()
-                .navigationTitle("Settings")
-                .alert(isPresented: $showRestartAlert) {
-                    Alert(title: Text("Restart Required"), message: Text("Please close and reopen the app to see changes."), dismissButton: .default(Text("OK")))
                 }
+                .padding()
+                
+                Section {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Cancel")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .padding()
+            }
+            .padding()
+            .navigationTitle("Settings")
+            .alert(isPresented: $showRestartAlert) {
+                Alert(title: Text("Restart Required"), message: Text("Please close and reopen the app to see changes."), dismissButton: .default(Text("OK")))
             }
         }
     }
-        
+
         
         private func deleteAll() {
             let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Location.fetchRequest()
